b6c3de0a8bc21477e4e0ea99cd3d16bba412bf44
Noah Daniels <ndaniels@mac.com>
2014-02-26 21:44:18 -0500

    remove debug printing

--- seeds.go
+++ seeds.go
@@ -5,7 +5,6 @@ import (
 	"strings"
 	"sync"
 	"bytes"
-  "fmt"
 )
 
 // SeedAlphaNums is a map to assign *valid* amino acid resiudes contiunous
@@ -179,7 +178,6 @@ func (ss *Seeds) Add(coarseSeqIndex int, corSeq *CoarseSeq) {
 func (ss Seeds) Lookup(kmer []byte, mem *[][2]uint) [][2]uint {
 	ss.lock.RLock()
 	// Don't use defer. It comes with a performance penalty in hot spots.
-  fmt.Println(string(kmer))
 	seeds := ss.Locs[ss.hashKmer(kmer)]
 	if seeds == nil {
 		ss.lock.RUnlock()
