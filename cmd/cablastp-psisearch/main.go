834ce464d95f9495ec3f5c76d2da6876d310387d
Noah Daniels <ndaniels@mac.com>
2014-02-27 22:39:54 -0500

    namespace, and missing xml (lots of repeated code in xml.go and in the various main files could be factored out)

--- cmd/cablastp-psisearch/main.go
+++ cmd/cablastp-psisearch/main.go
@@ -120,7 +120,7 @@ func main() {
   }
 
   cablastp.Vprintln("\nBlasting query on coarse database...")
-  if err := blastCoarse(db, Reduce(inputFastaQuery), buf); err != nil {
+  if err := blastCoarse(db, cablastp.Reduce(inputFastaQuery), buf); err != nil {
     fatalf("Error blasting coarse database: %s\n", err)
   }
 
